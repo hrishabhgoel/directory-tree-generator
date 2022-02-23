37d169f4000614ef37b1bbf2ea9f506c129942ef
Hrishabh Goel <hrishabh.w21@gmail.com>
2022-02-23 03:14:51 -0800

    second main commit

--- dtg.py
+++ dtg.py
@@ -4,4 +4,87 @@ from cv2 import QT_STYLE_OBLIQUE
 from docopt import *
 from colr import color
 from PIL import Image, ImageDraw, ImageFont
-import random
+
+# ------------------ colors 
+colors = ['#728239', '#fd742d', '#4a9396', '#cb7f07', '#c7bab1', '#a6b401', '#c1dee7', '#6389df', '#f891a0']
+borderTopBottom = '-' * 120
+
+# ------------------ methods
+# @desc   fileSearch in a dir func
+# @params _dirpath: path of _dir to search
+#         filename: name of file to search       
+# @return allDirs: a string of all diretories 
+#                   containing filename 
+def fileSearch(_dirpath, filename):
+     global colors
+     i = 0
+     global borderTopBottom
+     allDirs = borderTopBottom
+
+     for root, dirs, files in os.walk(_dirpath):
+          for file in files:
+               if filename in file:
+                    if i == len(colors):
+                         i = 0
+                    allDirs += color("\n" + os.path.join(root,file), fore=colors[i])
+                    i += 1
+               
+     allDirs += borderTopBottom
+     return allDirs
+
+# @desc   gentree of a dir func
+# @params path: dir to genTree
+# @return treePath: tree of path 
+def genTree (path): 
+     global colors
+     i = 0
+     global borderTopBottom 
+     treePath = borderTopBottom + '\n'
+
+     for root, dirs, files in os.walk(path):
+
+          root = root.replace(path, "")
+
+          # determine the level by counting the os separators
+          level = root.count(os.sep)
+
+          # create branch for each file in directory
+          if level == 0:
+               treePath += color(path + "\n", fore='red')
+
+               for file in files:
+                    treePath += color("  "*level + "|--", 'red')
+                    treePath += color(file + "\n", colors[i])
+                    i += 1
+          else:
+               treePath += color("|" + "--"*level + root + "\n", fore='red', style='bright')
+
+               # create branch for each file in directory
+               for file in files:
+                    if i == len(colors): i = 0
+                    treePath += color("|" + "  "*level + "|--", 'red')
+                    treePath += color(file + "\n", colors[i])
+                    i += 1
+
+               treePath += color("|\n", fore='red')
+     treePath += borderTopBottom
+     return treePath
+
+# ------------------ how to use
+howToUse = ''' 
+     Usage: 
+     dtg.py -t <DIRECTORY_PATH>
+     dtg.py -s <DIRECTORY_PATH> <FILE_NAME>
+
+''' 
+
+# ------------------ main
+args = docopt(howToUse)
+
+if args['-t']:
+    output = genTree(args['<DIRECTORY_PATH>'])
+    print(output)
+
+if args['-s']:
+    output = fileSearch(args['<DIRECTORY_PATH>'], args['<FILE NAME>'])
+    print(output)
--- todo.md
+++ todo.md
@@ -3,10 +3,10 @@
 0. create file dtg.py
 1. import libraries
 2. determine colors
-3. create function to searchFile
-4. create function to getWidth
-5. create function to getHeight
-6. create function to genTree
+3. create function to fileSearch
+4. create function to genTree
+5. create function to getWidth
+6. create function to getHeight
 7. create function to captureScreenshot
 8. create usage screen/menu driven prpogram
 9. test
